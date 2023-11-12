a5139aa2de85c5040f1dfb15306ba5143aef556b
Matias <matiasvaca993@gmail.com>
2023-11-12 02:17:50 -0300

    f

--- webdriverio/test/specs/verifyChaiJsWebsiteLinks.js
+++ webdriverio/test/specs/verifyChaiJsWebsiteLinks.js
@@ -18,34 +18,34 @@ describe('Verify whether chaijs.com links on ' +
         assert.equal(title, 'Chai', 'title is not Chai');
     });
 
-    // it('User should be able to navigate to ' +
-    //     'Guide Page of chaijs.com', () => {
-    //     browser.url('https://www.chaijs.com');
-    //     const element = $('//*[@href="/guide"]');
-    //     element.click();
-    //     browser.pause(5000);
-    //     const title = browser.getTitle();
-    //     // assert(title === 'Getting Started Guide - ' +
-    //     //     'Chai', "title is not 'Getting Started Guide - Chai'");
+     it('User should be able to navigate to ' +
+         'Guide Page of chaijs.com', () => {
+         browser.url('https://www.chaijs.com');
+         const element = $('//*[@href="/guide"]');
+         element.click();
+         browser.pause(5000);
+         const title = browser.getTitle();
+         // assert(title === 'Getting Started Guide - ' +
+         //     'Chai', "title is not 'Getting Started Guide - Chai'");
 
-    //     expect(title).to.be.a('string').
-    //         and.equal('Getting Started Guide - ' +
-    //         'Chai', "title is not 'Getting Started Guide - Chai'");
-    // });
+         expect(title).to.be.a('string').
+             and.equal('Getting Started Guide - ' +
+             'Chai', "title is not 'Getting Started Guide - Chai'");
+     });
 
-    // it('User should be able to navigate to ' +
-    //     'API Page of chaijs.com', () => {
-    //     browser.url('https://www.chaijs.com');
-    //     const element = $('//*[@href="/api"]');
-    //     element.click();
-    //     browser.pause(5000);
-    //     const title = browser.getTitle();
-    //     // assert(title === 'Introduction - Chai',
-    //     //     "title is not 'Introduction - Chai'");
-    //     title.should.be.a('string').and.equal(
-    //         'Introduction - Chai',
-    //         "title is not 'Introduction - Chai'"
-    //     )
+     it('User should be able to navigate to ' +
+         'API Page of chaijs.com', () => {
+         browser.url('https://www.chaijs.com');
+         const element = $('//*[@href="/api"]');
+         element.click();
+         browser.pause(5000);
+         const title = browser.getTitle();
+         // assert(title === 'Introduction - Chai',
+         //     "title is not 'Introduction - Chai'");
+         title.should.be.a('string').and.equal(
+             'Introduction - Chai',
+             "title is not 'Introduction - Chai'"
+         )
 
     // });
 
